375ba80070d3375f159e82cf9edeada9bbbf932c
Chinni Raja <chinnibenz143@gmail.com>
2017-06-26 12:28:52 +0800

    first commit from eclipse egit

---
+++ testApp/src/testApp/testApp.java
@@ -0,0 +1,10 @@
+package testApp;
+
+public class testApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Testing egit with eclipse");
+	}
+
+}
