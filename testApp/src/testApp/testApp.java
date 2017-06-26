7e23b3858f015ad8af72513214898e1c363ff202
Chinni Raja <chinnibenz143@gmail.com>
2017-06-26 12:49:19 +0800

    second commit

--- testApp/src/testApp/testApp.java
+++ testApp/src/testApp/testApp.java
@@ -4,7 +4,7 @@ public class testApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println("Testing egit with eclipse");
+      System.out.println("Testing egit with eclipse second change");
 	}
 
 }
